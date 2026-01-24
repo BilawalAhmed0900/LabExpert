09bf85f5070d9434af388d91793ff04805c5a7f0
Bilawal Ahmed <blwal7057@gmail.com>
2026-01-24 13:02:25 +0100

    Delete everything

--- android/app/src/main/java/com/bilawalahmed/lab_expert/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.bilawalahmed.lab_expert;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- lib/Functions/cast_array.dart
+++
@@ -1,5 +0,0 @@
-List<T> castList<T>(List<dynamic> list) {
-  return list.map((dynamic e) {
-    return e as T;
-  }).toList();
-}
